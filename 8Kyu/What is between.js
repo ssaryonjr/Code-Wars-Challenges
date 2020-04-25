cdfd8779aaa69340ddab1041cf249875144058de
Sam Saryon <ssaryonjr@gmail.com>
2020-04-25 11:05:26 -0700

    daily coding challenge!

--- 8Kyu/What is between.js
+++ 8Kyu/What is between.js
@@ -3,7 +3,7 @@
 
 // Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
-// For example
+// For example:
 
 // a = 1
 // b = 4
