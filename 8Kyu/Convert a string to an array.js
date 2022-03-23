e4917875bf38936c4140bcebcba0ce1ff473aaba
Sam Saryon <ssaryonjr@gmail.com>
2022-03-23 07:15:22 -0700

    daily code challenge

---
+++ 8Kyu/Convert a string to an array.js
@@ -0,0 +1,14 @@
+// Convert a string to an array - 8 Kyu
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they"]
+function stringToArray(string){
+    return string.split(' ')
+        // code code code
+    
+    }
