0073a9a39ca0cdc267615ab9f0f99b1782e5cf35
Sam Saryon <ssaryonjr@gmail.com>
2022-03-31 07:42:12 -0700

    daily coding challenge

---
+++ 8Kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,8 @@
+// Implement a function which multiplies two numbers. - 8 Kyu
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+
+function multiply(num1, num2){
+    return num1 * num2
+  }
+  
