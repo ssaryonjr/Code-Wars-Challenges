61ba522edd1f774d40738457b94980a8ad50c7aa
Sam Saryon <ssaryonjr@gmail.com>
2022-04-03 12:12:12 -0700

    more code wars

---
+++ 8Kyu/Volume of Cuboid.js
@@ -0,0 +1,10 @@
+// Volume of Cuboid - 8 Kyu
+//https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
