c705b722c2d9507a8742ddc7fd6f3e0a4e587242
Sam Saryon <ssaryonjr@gmail.com>
2022-04-08 22:34:08 -0700

    Added another challenge

---
+++ 8Kyu/Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,8 @@
+//Convert boolean values to strings 'Yes' or 'No'. - 8 Kyu
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
