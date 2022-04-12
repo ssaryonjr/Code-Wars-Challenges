35f36b64cd2cdad7ca1171d94fa51e00b70c556a
Sam Saryon <ssaryonjr@gmail.com>
2022-04-12 00:58:22 -0700

    More coding problems

--- testing.js
+++ testing.js
@@ -1,67 +1,7 @@
-//'x' Marks the spot - 7 Kyu
-//https://www.codewars.com/kata/5777fe3f355edbf0a5000d11/
-
-// #'x' marks the spot
-
-// ##Task:
-
-// Given a two dimensional array, return the co-ordinates of 'x'.
-
-// If 'x' is not inside the array, or if 'x' appears multiple times, return []
-
-// The co-ordinates should be zero indexed.
-
-// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
-
-// Example test cases:
-
-// 'Return an empty array if input is an empty array' => []
-
-// [] 
-
-// 'Return an empty array if no x found' => []
-
-// [
-//   ['o', 'o'],
-//   ['o', 'o']
-// ]
-
-// 'Return an empty array if more than one x found' => []
-
-// [
-//   ['x', 'o'],
-//   ['o', 'x']
-// ]
-
-// 'Return [0,0] when x at top left' => [0, 0]
-
-// [
-//   ['x', 'o'],
-//   ['o', 'o']
-// ]
-
-// 'Return [4,6] for the example below' => [4, 6]
-
-// [
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
-//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
-// ]
-
-const xMarksTheSpot = (input) => {
-    let result = []
-
-    for (let i = 0; i < input.length; i++){
-        for (let j = 0; j < input[i].length; j++){
-          if (input[i][j] == 'x'){
-             result.push([i,j])
-          }
-        }
-    } 
-
-return input.length > 2 || input.length == 0 ? [] : result
-    
+function camelize(str){
+  return str
+  .split('-')
+  .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
 }
+
+console.log(camelize('list-style-image'))
