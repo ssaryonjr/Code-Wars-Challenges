1f3414e05e9e2ae55833c8ff966ef7a0eb85f83c
Sam Saryon <ssaryonjr@gmail.com>
2022-04-18 21:28:58 -0700

    Grind dont stop still doing codewars!

--- testing.js
+++ testing.js
@@ -1,1 +1,18 @@
-console.log('hi')
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+
+
+  function multiply(obj){
+      for (let key in obj){
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+          }
+      }
+  }
+
+  multiply(menu)
+  console.log(menu)
