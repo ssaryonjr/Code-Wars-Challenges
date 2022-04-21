483d53a75d6a1e9b9f5e24eb2c51a1f4ebace5d6
Sam Saryon <ssaryonjr@gmail.com>
2022-04-20 20:10:56 -0700

    more grinding

--- testing.js
+++ testing.js
@@ -1,18 +1,1 @@
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-  };
-  
-
-
-  function multiply(obj){
-      for (let key in obj){
-        if (typeof obj[key] == 'number') {
-            obj[key] *= 2;
-          }
-      }
-  }
-
-  multiply(menu)
-  console.log(menu)
+console.log('hi')
