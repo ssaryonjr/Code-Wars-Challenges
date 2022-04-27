29dcb5432d3e99bf3b6b7f71634319ec898ddc3e
Sam Saryon <ssaryonjr@gmail.com>
2022-04-26 20:10:08 -0700

    new problem

---
+++ 8Kyu/lost without map.js
@@ -0,0 +1,13 @@
+// Lost without map - 8 Kyu
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+// Hide Kata Description
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    return x.map(n => n * 2);
+  }
