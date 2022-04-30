9910a63c266736514d0590f45b4b4fc2f8042b05
Sam Saryon <ssaryonjr@gmail.com>
2022-04-30 16:23:25 -0700

    more pushes of codewars

---
+++ 7Kyu/string ends with.js
@@ -0,0 +1,16 @@
+// String ends with? - 7 Kyu  
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+    return str.endsWith(ending)
+}
+
+console.log(solution('hello world', 'o'))
+
