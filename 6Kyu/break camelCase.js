88dc274ad4d54acead1651723955eaa8c4537743
Sam Saryon <ssaryonjr@gmail.com>
2022-05-24 00:25:38 -0700

    daily code challenge

---
+++ 6Kyu/break camelCase.js
@@ -0,0 +1,21 @@
+// Break camelCase - 6 Kyu
+// https://www.codewars.com/kata/5208f99aee097e6552000148
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+    string = string.split('').map(function (el) {
+      if (el === el.toUpperCase()) {
+        el = ' ' + el
+      }
+      return el
+    })
+    return string.join('')
+  }
+
+console.log(solution("camelCaseCase" ))
