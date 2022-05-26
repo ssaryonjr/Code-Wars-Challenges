c6d747078138ff839be8edb9481d21bf6bc937aa
Sam Saryon <ssaryonjr@gmail.com>
2022-05-26 15:44:50 -0700

    Daily coding challenge

---
+++ 8Kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,9 @@
+// You Can't Code Under Pressure #1 - 8 Kyu
+//https://www.codewars.com/kata/53ee5429ba190077850011d4
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2
+}
+
