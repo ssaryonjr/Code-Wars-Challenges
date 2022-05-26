33fb3eaaa3bf4c331e0c8d33b48b18878a7501c6
Sam Saryon <ssaryonjr@gmail.com>
2022-05-25 18:07:53 -0700

    daily coding challenge

--- testing.js
+++ testing.js
@@ -1,1 +1,18 @@
-console.log('test')
+let numbers = [7, 21, 54, 32, 5];
+
+function iterate(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 15){
+            console.log(arr[i])
+        } 
+    }
+}
+
+
+// iterate(numbers)
+
+function filter(arr){
+    return arr.filter(num => num < 21)
+}
+
+console.log(filter(numbers))
