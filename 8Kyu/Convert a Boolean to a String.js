a292399f1823559750400c63bbc8954bafe8e1c1
Sam Saryon <ssaryonjr@gmail.com>
2022-05-28 15:58:17 -0700

    daily coding challenge

---
+++ 8Kyu/Convert a Boolean to a String.js
@@ -0,0 +1,12 @@
+// Convert a Boolean to a String - 8 Kyu
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    return b ? 'true' : 'false'
+  }
+  
+  console.log(booleanToString(false))
