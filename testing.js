4f389a696a0c971838daccc8e2b407dea5a6cf82
Sam Saryon <ssaryonjr@gmail.com>
2022-06-05 07:32:52 -0700

    daily coding challenge

--- testing.js
+++ testing.js
@@ -1,17 +1,1 @@
-// If you can't sleep, just count sheep!! - 8 Kyu
-// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
-
-// Task:
-// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
-
-
-const countSheep = function (num){
-    let text = '';
-    for (let i = 1; i <= num; i++){
-        text += i + ' sheep...'
-    }
-    return text
-}
-
-console.log(countSheep(3))
-
+console.log('test')
