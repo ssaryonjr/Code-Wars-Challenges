4bf77d729a8dece11cb5062bc38567c482c01246
Sam Saryon <ssaryonjr@gmail.com>
2022-06-08 01:13:13 -0700

    daily coding challenge

---
+++ 8Kyu/Transportation on vacation.js
@@ -0,0 +1,22 @@
+// Transportation on vacation - 8 Kyu 
+// https://www.codewars.com/kata/568d0dd208ee69389d000016
+
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+    let total = d * 40
+    if (d >= 7){
+        total -= 50
+    } else if (d >= 3){
+        total -= 20
+    }
+    return total
+}
+
+  console.log(rentalCarCost(4))
