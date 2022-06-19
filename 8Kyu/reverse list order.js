af666d31cc08556ab7fb335d0bfe3534f2b394d5
Sam Saryon <ssaryonjr@gmail.com>
2022-06-18 19:13:13 -0700

    daily coding challenge

---
+++ 8Kyu/reverse list order.js
@@ -0,0 +1,12 @@
+// Reverse List Order - 8 Kyu 
+// https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
+
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+function reverseList(list) {
+    return list.reverse()
+}
--- testing.js
+++ testing.js
@@ -1,1 +1,6 @@
-console.log('test')
+function test(str){
+    return new Set(str.toLowerCase()).size == str.length
+}
+
+console.log(test("Dermatoglyphics"))
+console.log(test("moOse"))
