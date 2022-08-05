a11474e814e772c9b0dc7e6adeecc2aff6ddba2a
Sam Saryon <ssaryonjr@gmail.com>
2022-08-04 17:14:26 -0700

    daily coding challenge

---
+++ 6Kyu/count the smiley faces.js
@@ -0,0 +1,18 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+    var smileys = [":)",";)",":-)",";-)",";~)",":~)",":D",";D",":-D",":~D",";-D",";~D"];
+    var count = 0;
+    
+    for (var i=0; i<arr.length; i++){
+      for (var j=0; j<smileys.length; j++){
+        if (arr[i]===smileys[j]){
+          count++;
+        }
+      }
+      }
+    return count;
+    }
+
+}
+
+console.log(countSmileys([':D',':~)',';~D',':)']))
