8daaeee6300fab697d74cb76e97b90c07cc5542b
Sam Saryon <ssaryonjr@gmail.com>
2022-08-17 17:17:50 -0700

    daily coding challenge

--- testing.js
+++ testing.js
@@ -1,10 +1,1 @@
-function reverseLetter(str) {
-return str
-.toLowerCase()
-.split('')
-.reverse()
-.filter(l => Array.from('abcdefghijklmnopqrstuvwxyz').includes(l))
-.join('')
-}
-
-console.log(reverseLetter("ultr53o?n")) //nortlu
+console.log('test')
