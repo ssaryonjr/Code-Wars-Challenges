794d24a7d4cfdb62f4bcf1098df0a7748fcf6457
Sam Saryon <ssaryonjr@gmail.com>
2022-08-22 17:45:09 -0700

    daily coding challenge

---
+++ 8Kyu/Reverse string.js
@@ -0,0 +1,10 @@
+// Reversed Strings - 8 Kyu
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    return str.split('').reverse().join('')
+  }
